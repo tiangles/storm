1d508a5a07851a0da68a187c6c2e960e0d67ec17
btian <tiangles@outlook.com>
2017-06-30 17:19:05 +0800

    add user event session

--- server/AdminServices/event/models.py
+++ server/AdminServices/event/models.py
@@ -3,7 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.sessions.base_session import (
+    AbstractBaseSession, BaseSessionManager,
+)
 
 class UserEvent(models.Model):
     type = models.CharField(max_length=64, blank=False)
@@ -13,3 +16,22 @@ class UserEvent(models.Model):
 
     class Meta:
         db_table = 'auth_user_event'
+
+
+class SessionManager(BaseSessionManager):
+    use_in_migrations = True
+
+
+class UserEventSession(AbstractBaseSession):
+    objects = SessionManager()
+    created_date = models.DateTimeField(verbose_name=_('Created Date'), auto_now=True)
+    user = models.ForeignKey(User)
+
+    class Meta(AbstractBaseSession.Meta):
+        db_table = 'auth_user_event_session'
+
+
+    @classmethod
+    def get_session_store_class(cls):
+        from django.contrib.sessions.backends.db import SessionStore
+        return SessionStore
